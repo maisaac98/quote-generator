1113525c549af32e9b4324d47d4c3a5c1f7e7778
Isaac Ma <maisaac98@gmail.com>
2022-06-05 13:20:28 +1000

    Added more quotes to non-API retrieved quotes

--- quotes.js
+++ quotes.js
@@ -7,5 +7,53 @@ const localQuotes = [
     text: 'Call out for insight, and cry aloud for understanding',
     author: 'Proverbs 2:3',
   },
+  {
+    text: 'But he who sins against me injures himself; All those who hate me love death',
+    author: 'Proverbs 8:36',
+  },
+  {
+    text: 'How then does a man gain the essence of wisdom? We cross the threshold of true knowledge when we live in complete awe and adoration of God. Stubborn know-it-alls will never stop to do this, for they scorn true wisdom and knowledge',
+    author: 'Proverbs 1:7',
+  },
+  {
+    text: 'Do not be wise in your own eyes; Fear the Lord [with reverent awe and obedience] and turn [entirely] away from evil',
+    author: 'Proverbs 3:7',
+  },
+  {
+    text: 'Wisdom is the most important thing; so get wisdom. If it costs everything you have, get understanding',
+    author: 'Proverbs 4:7',
+  },
+  {
+    text: 'Those who find true wisdom obtain the tools for understanding, the proper way to live, for they will have a fountain of blessing pouring into their lives. To gain the riches of wisdom is far greater than gaining the wealth of the world. As wisdom increases, a great treasure is imparted, greater than many bars of refined gold',
+    author: 'Proverbs 3:13-14',
+  },
+  {
+    text: 'For the Lord gives wisdom; from his mouth come knowledge and understanding',
+    author: 'Proverbs 2:6',
+  },
+  {
+    text: 'Fear of the Lord is the foundation of wisdom. Knowledge of the Holy One results in good judgment',
+    author: 'Proverbs 9:10',
+  },
+  {
+    text: 'A wise son accepts his father’s discipline, but a scoffer does not listen to rebuke',
+    author: 'Proverbs 13:1',
+  },
+  {
+    text: 'When you act with presumption, convinced that you’re right, don’t be surprised if you fall flat on your face! But walking in humility helps you to make wise decisions',
+    author: 'Proverbs 11:2',
+  },
+  {
+    text: 'Your boast becomes a prophecy of a future failure. The higher you lift up yourself in pride, the harder you’ll fall in disgrace',
+    author: 'Proverbs 16:18',
+  },
+  {
+    text: 'It is better to be of a lowly spirit with the poor than to divide the spoil with the proud',
+    author: 'Proverbs 16:19',
+  },
+  {
+    text: 'Pride comes before a disaster, but humility comes before respect',
+    author: 'Proverbs 18:12',
+  },
   
 ];
